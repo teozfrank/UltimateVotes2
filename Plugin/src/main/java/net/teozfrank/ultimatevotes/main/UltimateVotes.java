0dc2f37600f30cdd9a30a717fe9bca3d13705f7d
teozfrank <unconfigured@null.spigotmc.org>
2020-08-24 12:25:57 +0100

    - Update to 1.16.2

--- Plugin/src/main/java/net/teozfrank/ultimatevotes/main/UltimateVotes.java
+++ Plugin/src/main/java/net/teozfrank/ultimatevotes/main/UltimateVotes.java
@@ -247,7 +247,7 @@ public class UltimateVotes extends JavaPlugin {
 
         String[] legacyVersions =  { "v1_8_R1", "v1_8_R2", "v1_8_R3", "v1_9_R1", "v1_9_R2",
                 "v1_10_R1", "v1_10_R1", "v1_11_R1", "v1_11_R1", "v1_12_R1" };
-        String[] latestVersions = {"v1_13_R1",  "v1_13_R2", "v1_14_R1", "v1_15_R1", "v1_16_R1"};
+        String[] latestVersions = {"v1_13_R1",  "v1_13_R2", "v1_14_R1", "v1_15_R1", "v1_16_R1", "v1_16_R2"};
 
         boolean legacy = false;
         boolean latest = false;
@@ -303,7 +303,7 @@ public class UltimateVotes extends JavaPlugin {
 
         String[] legacyVersions =  { "v1_8_R1", "v1_8_R2", "v1_8_R3", "v1_9_R1", "v1_9_R2",
                 "v1_10_R1", "v1_10_R1", "v1_11_R1", "v1_11_R1", "v1_12_R1", "v1_13_R1",  "v1_13_R2" };
-        String[] latestVersions = {"v1_14_R1", "v1_15_R1", "v1_16_R1"};
+        String[] latestVersions = {"v1_14_R1", "v1_15_R1", "v1_16_R1", "v1_16_R2"};
 
         boolean legacy = false;
         boolean latest = false;
